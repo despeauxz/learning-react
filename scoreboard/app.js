1b450458b9d43a91ac6a3887c01ac57f82a3baf8
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-19 16:59:35 +0100

    binding (this) to its methods and increment player score

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -44,6 +44,12 @@ const Player = (props) => {
 class Counter extends React.Component {
     state = {
         score: 0
+    };
+
+    incrementScore() {
+        this.setState({
+            score: this.state.score + 1
+        });
     }
 
     render() {
@@ -51,7 +57,7 @@ class Counter extends React.Component {
             <div className="counter">
                 <button className="counter-action decrement">-</button>
                 <span className="counter-score">{ this.state.score }</span>
-                <button className="counter-action increment">+</button>
+                <button className="counter-action increment" onClick={this.incrementScore.bind(this)}>+</button>
             </div>
         );
     }
