9e9803be22266bc5a2edf0282eecf9f69fd08bc4
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-19 14:52:47 +0100

    working with JSX and variable templating in React

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,20 +1,12 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is my first title' },
-    'My First React Element'
-);
+const name = 'Godwin';
+const title = 'My First React Element';
+const desc = 'I just learned how to create a React node and render it into the DOM';
 
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM'
-);
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+        <h1>My name is { name } and this is { title }</h1>
+        <p>{ desc }</p>
+    </header>
 );
 
 ReactDOM.render(
